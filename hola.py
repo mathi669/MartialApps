d76b6301d571fa6c8d5a98c64dda35848d772f6b
Raffaelsrc <ra.rodriguezc@duocuc.cl>
2024-04-22 23:26:49 -0400

    Add main line

--- hola.py
+++ hola.py
@@ -1,7 +1,19 @@
-from flask import Flask
-
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 @app.route("/hello")
 def hola():
     return "<h1>Hola mundo</h1>"
+
+user = {
+    "id": 1,
+    "name": "John Doe",
+    "email": "john.doe@example.com"
+}
+
+@app.route('/user', methods=['GET'])
+def get_user():
+    return jsonify(user)
+
+if __name__ == 'main':
+    app.run(debug=True)
