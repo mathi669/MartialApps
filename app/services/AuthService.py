3ec903f8939491c8de00d0c8fd418fb5d2bc2c49
JosephWebs <jose_luis967@hotmail.com>
2024-05-31 15:05:06 -0400

    cambios y endpoints

--- app/services/AuthService.py
+++ app/services/AuthService.py
@@ -1,23 +1,32 @@
 from app.database.mysql_conection import get_conection
 from app.models.user import User
 
-class AuthService():
-    
+
+class AuthService:
+
     @classmethod
     def login_user(cls, user):
         try:
-            authenticated_user = False
             conn = get_conection()  # Obtener conexión a la base de datos
             print(user.dc_correo_electronico)
             with conn.cursor() as cursor:
-                cursor.callproc('sp_AuthUser', (user.dc_correo_electronico, user.dc_contrasena))
+                cursor.callproc(
+                    "sp_AuthUser", (user.dc_correo_electronico, user.dc_contrasena)
+                )
                 result = cursor.fetchone()
                 print(result)
-                if result[0] == 1:
-                    authenticated_user = user
+
+                if (
+                    result and result[0]
+                ):  # Verificar si el primer elemento indica si el usuario existe
+                    user.id_usuario = result[
+                        1
+                    ]  # Asignar el ID del usuario (segundo elemento)
+                    return user
+                else:
+                    return None
         except Exception as e:
             print("Error en el método login_user:", e)
             raise  # Vuelve a lanzar la excepción original
         finally:
             conn.close()  # Asegúrate de cerrar la conexión
-        return authenticated_user
