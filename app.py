95f34bbf6e5b3f10e622b57f8021f6cd194c93ae
mathi669 <matias.luarte@hotmail.com>
2024-06-02 16:40:23 -0400

    add new port

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from config import config
 from app.routes import routes
 from app.routes.routes import routes  # Importa el blueprint routes correctamente
 from flask_cors import CORS
+from decouple import config as env_config
 
 
 app = Flask(__name__)
@@ -11,4 +12,4 @@ if __name__ == "__main__":
     CORS(app, resources={r"/*": {"origins": "*"}}) 
     app.config.from_object(config['development'])
     app.register_blueprint(routes, url_prefix='/')
-    app.run(debug=True, host='0.0.0.0', port=8000, threaded=True)
+    app.run(debug=True, host='0.0.0.0', port=env_config('PORT'), threaded=True)
