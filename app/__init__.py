454cee97bd43f68af3494f573ee11617bfe0d25e
mathi669 <matias.luarte@hotmail.com>
2024-06-11 21:40:32 -0400

    try to deploy 2

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,1 @@
-from flask import Flask
-from .routes import routes
-
-app = Flask(__name__)
-
-def init_app(config):
-    app.config.from_object(config)
-
-    app.register_blueprint(routes.main, url_prefix='/')
-
-    return app
 
