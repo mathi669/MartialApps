85e6ae874abc7608e3677eaf8d25247b956f6da6
Leandro Paredes <leandro@hicapps.com>
2024-06-14 13:22:53 -0400

    fixes

--- config.py
+++ config.py
@@ -1,11 +1,22 @@
-from decouple import config
+from decouple import config as env_config
 
 class Config:
-    SECRET_KEY=config('SECRET_KEY')
-    
+    SECRET_KEY = env_config('SECRET_KEY')
+    MYSQL_HOST = env_config('MYSQL_HOST')
+    MYSQL_USER = env_config('MYSQL_USER')
+    MYSQL_PASSWORD = env_config('MYSQL_PASSWORD')
+    MYSQL_DB = env_config('MYSQL_DB')
+    PORT_DB = env_config('PORT_DB')
+    JWT_SECRET_KEY = env_config('JWT_SECRET_KEY')
+    JWT_KEY = env_config('JWT_KEY')
+    IMG_BB_KEY = env_config('IMG_BB_KEY')
+    MAIL_USERNAME = env_config('MAIL_USERNAME')
+    MAIL_PASSWORD = env_config('MAIL_PASSWORD')
+
 class DevelopmentConfig(Config):
-    DEBUG=True
+    DEBUG = True
 
 config = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'default': DevelopmentConfig
 }
